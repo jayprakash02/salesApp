088c23bba5d97cbe0b8fc45c08e16a909bb74af9
kamtendra <skamtendra@gmail.com>
2023-04-08 15:32:33 +0530

    f

--- api/models.py
+++ api/models.py
@@ -128,16 +128,16 @@ class Distributor(models.Model):
 
 class Order(models.Model):
     # store = models.ForeignKey(Store, on_delete=models.CASCADE, related_name='orders')
-    pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE,null=True)
+    pharmacy = models.ForeignKey(Pharmacy, on_delete=models.CASCADE)
     product_name = models.ManyToManyField(Product)
     sales_rep = models.ForeignKey(SalesRepresentative, on_delete=models.CASCADE, related_name='orders')
-    order_book_image = models.ImageField(upload_to='order_book_images',null=True)
-    distributer = models.ForeignKey(Distributor, on_delete=models.CASCADE,null=True)
+    order_book_image = models.ImageField(upload_to='order_book_images')
+    distributer = models.ForeignKey(Distributor, on_delete=models.CASCADE)
     delivery_date = models.DateField()
     # payment_terms = models.CharField(max_length=255)
     shipping_address = models.CharField(max_length=255)
     # notes = models.TextField()
-    order_date = models.DateTimeField(auto_now_add=True,null=True)
+    order_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order for {self.pharmacy.name} ({self.delivery_date})"
